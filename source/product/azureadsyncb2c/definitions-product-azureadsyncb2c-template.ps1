35b9eb5060993e2aebc26450296a5a1f6033331e
David Walker <dwalker3rd@gmail.com>
2022-07-20 10:54:16 +0000

    task name fix

--- source/product/azureadsyncb2c/definitions-product-azureadsyncb2c-template.ps1
+++ source/product/azureadsyncb2c/definitions-product-azureadsyncb2c-template.ps1
@@ -6,7 +6,7 @@ $definitionsPath = $global:Location.Definitions
 . $definitionsPath\classes.ps1
 
 $global:Product = $global:Catalog.Product.AzureADSyncB2C
-$global:Product.DisplayName = "$($global:Overwatch.Name) $($global:Product.Name) for $($global:Platform.DisplayName)"
+$global:Product.DisplayName = "$($global:Overwatch.Name) $($global:Product.Name) for $($global:Platform.Name)"
 $global:Product.TaskName = $global:Product.DisplayName
 $global:Product.Description = "Syncs Azure AD users to Azure AD B2C."
 $global:Product.HasTask = $true
