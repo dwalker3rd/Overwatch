9fbba719822833f08d9aa2fdb49c987bbd45d8f1
David Walker <dwalker3rd@gmail.com>
2022-07-25 12:03:32 +0000

    added comments for config os

--- source/os/windowsserver/config-os-windowsserver-template.ps1
+++ source/os/windowsserver/config-os-windowsserver-template.ps1
@@ -8,22 +8,27 @@ $global:WarningPreference = "SilentlyContinue"
 $message = "<Configuring Powershell remoting <.>48> PENDING"
 Write-Host+ -NoTrace -NoTimestamp -NoNewLine -Parse $message -ForegroundColor Blue,DarkGray,DarkGray
 
+# this node is assumed to be the Overwatch controller for this platforminstance
 $thisNode = $env:COMPUTERNAME.ToLower()
 
 try {
 
+    # configure basic powershell remoting config (for this node)
     $ignoreOutput = winrm quickconfig
     $ignoreOutput = enable-psremoting -skipnetworkprofilecheck -force
     $ignoreOutput = set-netfirewallrule -name "WINRM-HTTP-In-TCP-PUBLIC" -RemoteAddress Any
 
+    # enable WSMan Credssp for both Server and Client (for this node)
     $ignoreOutput = Enable-WSManCredSSP -Role Server -Force
     $ignoreOutput = Enable-WSManCredSSP -Role Client -DelegateComputer $thisNode -Force
 
+    # configure local group credssp policy (for this node)
     $ignoreOutput = New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name AllowFreshCredentials -Force
     $ignoreOutput = New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentials -Name 1 -Value * -PropertyType String
     $ignoreOutput = New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name AllowFreshCredentialsWhenNTLMOnly -Force
     $ignoreOutput = New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -Name 1 -Value * -PropertyType String
 
+    # configure trusted hosts (for this node)
     $trustedHosts = Get-Item WSMan:\localhost\Client\TrustedHosts
     foreach ($node in (pt nodes -k)) {
         if ($node -notin $trustedHosts) {
@@ -31,6 +36,7 @@ try {
         }
     }
 
+    # enable WSMan Credssp for Server (for remote nodes)
     $psSessions = Get-PsSession+ -ComputerName (pt nodes -k | Where-Object {$_ -ne $thisNode})
     $ignoreOutput = Invoke-Command -ScriptBlock {Enable-WSManCredSSP -Role Server -Force} -Session $psSessions
     $ignoreOutput = Remove-PSSession $psSessions
