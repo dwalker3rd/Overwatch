be2590a031add0965592cf1cbf7ab2110f449636
David Walker <dwalker3rd@gmail.com>
2022-12-18 22:27:36 +0000

    fix check for updating pssession configuration

--- source/core/config/config-ps-remoting.ps1
+++ source/core/config/config-ps-remoting.ps1
@@ -9,7 +9,6 @@ $global:WarningPreference = "SilentlyContinue"
 
     $message = "<Powershell remoting <.>48> PENDING"
     Write-Host+ -NoTrace -NoTimestamp -Parse $message -ForegroundColor Blue,DarkGray,DarkGray
-    Write-Host+
 
     # this node is assumed to be the Overwatch controller for this platforminstance
     $thisNode = $env:COMPUTERNAME.ToLower()
@@ -35,11 +34,11 @@ $global:WarningPreference = "SilentlyContinue"
             throw("Unable to update required PSSessionConfiguration `"$($global:PSSessionConfigurationName)`"")
         }
 
-        foreach ($node in (pt nodes -k)) {
-            # Enable-PSRemoting to create the PSSession configurations, if not present such as after an upgrade of PowerShell
-            # easiest way to enable psremoting remotely: psexec is part of the Microsoft SysInternals Suite
-            $psexecResults = . "$($global:Location.SysinternalsSuite)\psexec.exe" "\\$node" -h -s pwsh.exe -Command "Enable-PSRemoting -SkipNetworkProfileCheck -Force" # -accepteula -nobanner 2>&1
-        }
+        # foreach ($node in (pt nodes -k)) {
+        #     # Enable-PSRemoting to create the PSSession configurations, if not present such as after an upgrade of PowerShell
+        #     # easiest way to enable psremoting remotely: psexec is part of the Microsoft SysInternals Suite
+        #     $psexecResults = . "$($global:Location.SysinternalsSuite)\psexec.exe" "\\$node" -h -s pwsh.exe -Command "Enable-PSRemoting -SkipNetworkProfileCheck -Force" # -accepteula -nobanner 2>&1
+        # }
 
         foreach ($node in (pt nodes -k)) {
 
@@ -76,18 +75,21 @@ $global:WarningPreference = "SilentlyContinue"
                 -ScriptBlock {
                     $_psVersion = $PSVersionTable.PSVersion.Patch -eq 0 ? "$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor)" : $PSVersionTable.PSVersion
                     $_psRequiredConfiguration = Get-PSSessionConfiguration -Name $using:PSSessionConfigurationName -ErrorAction SilentlyContinue
-                    if ($null -ne $_psRequiredConfiguration -and $_psRequiredConfiguration.PSVersion -ne $_psVersion) {
+                    if ($null -ne $_psRequiredConfiguration -and $_psRequiredConfiguration.FileName -notmatch "^.*\\PowerShell\\$($_psVersion)\\.*$") {
+                        
                         Unregister-PSSessionConfiguration -Name $using:PSSessionConfigurationName | Out-Null
-                        $_psRequiredConfiguration = Get-PSSessionConfiguration -Name $using:PSSessionConfigurationName -ErrorAction SilentlyContinue
+                        
+                        # Enable-PSRemoting to recreate the PSSession configurations since we just unregistered PowerShell.7
+                        # easiest way to enable psremoting remotely: psexec is part of the Microsoft SysInternals Suite
+                        $psexecResults = . "$($global:Location.SysinternalsSuite)\psexec.exe" "\\$node" -h -s pwsh.exe -Command "Enable-PSRemoting -SkipNetworkProfileCheck -Force" # -accepteula -nobanner 2>&1
+
+                        # $_psRequiredConfiguration = Get-PSSessionConfiguration -Name $using:PSSessionConfigurationName -ErrorAction SilentlyContinue
+
                     }
                     # if ($null -eq $_psRequiredConfiguration) {
                     #     Register-PSSessionConfiguration -Name $using:PSSessionConfigurationName -Path $using:_psRequiredSessionConfigurationFile -Force -WarningAction SilentlyContinue -ErrorAction SilentlyContinue | Out-Null
                     # }
                 } 
-
-            # Enable-PSRemoting to recreate the PSSession configurations since we just unregistered PowerShell.7
-            # easiest way to enable psremoting remotely: psexec is part of the Microsoft SysInternals Suite
-            $psexecResults = . "$($global:Location.SysinternalsSuite)\psexec.exe" "\\$node" -h -s pwsh.exe -Command "Enable-PSRemoting -SkipNetworkProfileCheck -Force" # -accepteula -nobanner 2>&1
 
         }
 
@@ -127,14 +129,12 @@ $global:WarningPreference = "SilentlyContinue"
             }
         }
 
-        Write-Host+
         $message = "<Powershell remoting <.>48> CONFIGURED"
         Write-Host+ -NoTrace -NoTimestamp -Parse $message -ForegroundColor Blue,DarkGray,DarkGreen
 
     }
     catch {
 
-        Write-Host+
         $message = "<Powershell remoting <.>48> PENDING"
         Write-Host+ -NoTrace -NoTimestamp -Parse $message -ForegroundColor Blue,DarkGray,Red
 
