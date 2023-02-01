e444ac021300854dd6c4edfc20b67d399bc07718
David Walker <dwalker3rd@gmail.com>
2023-02-01 16:45:05 +0000

    ignore vmcontext config when -MinimumDefinitions

--- source/product/bginfo/definitions-product-bginfo-template.ps1
+++ source/product/bginfo/definitions-product-bginfo-template.ps1
@@ -16,40 +16,44 @@ $global:Product = $global:Catalog.Product.BgInfo
 $global:Product.DisplayName = "$($global:Overwatch.Name) $($global:Product.Name) for $($global:Platform.Name)"
 $global:Product.TaskName = $global:Product.DisplayName
 
-$azVmContext = Get-AzVmContext
-$azVmExtension = Get-AzVMExtension -ResourceGroupName $azVmContext.ResourceGroupName -VMName $env:COMPUTERNAME -Name $global:Product.Id
+if (!$MinimumDefinitions) {
 
-$global:Product.Config = @{}
-$global:Product.Config += @{
-    Extension = $azVmExtension
-    Registry = @{
-        Path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
-        Key = "BGInfo"
-    }
-    Location = @{
-        Data = "$($global:Location.Data)\bgInfo"
-        Plugins = "C:\Packages\Plugins"
-    }
-}
-$global:Product.Config.Location += @{ 
-    VmExtension = "$($global:Product.Config.Location.Plugins)\$($azVmExtension.Publisher).$($azVmExtension.ExtensionType)\$($azVmExtension.TypeHandlerVersion)" 
-}
-$global:Product.Config.Location += @{
-    Files = @{
-        Destination = @{
-            ConfigBgi = "$($global:Product.Config.Location.VmExtension)\config.bgi"
-            ConfigTxt = "$($global:Product.Config.Location.VmExtension)\config.txt"
+    $azVmContext = Get-AzVmContext
+    $azVmExtension = Get-AzVMExtension -ResourceGroupName $azVmContext.ResourceGroupName -VMName $env:COMPUTERNAME -Name $global:Product.Id
+
+    $global:Product.Config = @{}
+    $global:Product.Config += @{
+        Extension = $azVmExtension
+        Registry = @{
+            Path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+            Key = "BGInfo"
         }
-        Source = @{
-            ConfigBgi = "$($global:Product.Config.Location.Data)\config.bgi"
+        Location = @{
+            Data = "$($global:Location.Data)\bgInfo"
+            Plugins = "C:\Packages\Plugins"
         }
     }
-}
-$global:Product.Config += @{
-    CommandLine = @{
-        Executable = "$($global:Product.Config.Location.VmExtension)\bgInfo.exe"
-        Options = @("/nolicprompt","/timer:0")
+    $global:Product.Config.Location += @{ 
+        VmExtension = "$($global:Product.Config.Location.Plugins)\$($azVmExtension.Publisher).$($azVmExtension.ExtensionType)\$($azVmExtension.TypeHandlerVersion)" 
     }
+    $global:Product.Config.Location += @{
+        Files = @{
+            Destination = @{
+                ConfigBgi = "$($global:Product.Config.Location.VmExtension)\config.bgi"
+                ConfigTxt = "$($global:Product.Config.Location.VmExtension)\config.txt"
+            }
+            Source = @{
+                ConfigBgi = "$($global:Product.Config.Location.Data)\config.bgi"
+            }
+        }
+    }
+    $global:Product.Config += @{
+        CommandLine = @{
+            Executable = "$($global:Product.Config.Location.VmExtension)\bgInfo.exe"
+            Options = @("/nolicprompt","/timer:0")
+        }
+    }
+
 }
 
 return $global:Product
