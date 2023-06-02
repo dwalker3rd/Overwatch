6b4b126446a87de9644631c64afe07d65535f5f1
David Walker <dwalker@path.org>
2023-06-02 10:27:12 +0000

    +tableau rmt platform pre/postflight checks

---
+++ source/platform/tableaurmt/postflightchecks-platform-tableaurmt-template.ps1
@@ -0,0 +1,19 @@
+#region POSTFLIGHT
+
+    #region IncompletePreflightChecks
+
+        if (!$global:PreflightChecksCompleted) {
+
+            Test-SslProtocol $global:Platform.Uri.Host 
+
+        }
+        else {
+
+            $message = "  No postflight checks"
+            Write-Host+ -NoTrace -NoSeparator $message -ForegroundColor Yellow
+
+        }
+
+    #endregion IncompletePreflightChecks
+
+#endregion POSTFLIGHT
---
+++ source/platform/tableaurmt/preflightchecks-platform-tableaurmt-template.ps1
@@ -0,0 +1,14 @@
+#region PREFLIGHT
+
+Test-Connections
+Test-PSRemoting
+
+$heartbeat = Get-Heartbeat
+if ($heartbeat.IsOK) {
+    Test-SslProtocol $global:Platform.Uri.Host 
+}
+else {
+    $global:PreflightChecksCompleted = $false
+}
+
+#endregion PREFLIGHT
