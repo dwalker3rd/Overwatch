ee6c28096f8a2dc3f21b23f2f33ce3476bf91c7d
David Walker <dwalker3rd@gmail.com>
2023-08-27 13:17:45 +0000

    maintain disable messaging for stop/start

--- source/product/azureruncommand/azureruncommand.ps1
+++ source/product/azureruncommand/azureruncommand.ps1
@@ -6,7 +6,6 @@
 param(
     [Parameter(Mandatory=$false,Position=0)][string]$Command,
     [Parameter(Mandatory=$false)][string]$OverwatchController = $env:COMPUTERNAME,
-    # [Parameter(Mandatory=$false)][string]$ComputerName,
     [switch]$RunSilent,
     [switch]$DoubleHop,
     [switch]$NoDoubleHop,
@@ -18,14 +17,34 @@ param(
 )
 
 function Test-AzureRunCommand { 
-
-    Write-Host+ "[Test-AzureRunCommand] PENDING "
-
+    Write-Host+ "[$($MyInvocation.MyCommand)] PENDING "
     # enter any command here for testing Overwatch's AzureRunCommand with Azure Automation 
-    Send-TaskMessage -Id Monitor -Status Running -MessageType $PlatformMessageType.Intervention -Message "Intervention Test"
+    Write-Host+ "[$($MyInvocation.MyCommand)] FINISHED"
+}
+function Test-MessagingOnStop { 
+    Write-Host+ "[$($MyInvocation.MyCommand)] PENDING "
+    $global:PlatformMessageType.Values | Where-Object {$_ -notin ("UserNotification","Heartbeat")} | ForEach-Object {
+        Send-TaskMessage -Id Monitor -Status Testing -MessageType $_ -Message "Testing $($_.ToLower()) message via AzureRunCommand\$($MyInvocation.MyCommand)" | Out-Null
+    }
+    Write-Host+ "[$($MyInvocation.MyCommand)] FINISHED"
+}
+function Test-MessagingOnStart { 
+    Write-Host+ "[$($MyInvocation.MyCommand)] PENDING "
+    $global:PlatformMessageType.Values | Where-Object {$_ -notin ("UserNotification","Heartbeat")} | ForEach-Object {
+        Send-TaskMessage -Id Monitor -Status Testing -MessageType $_ -Message "Testing $($_.ToLower()) message via AzureRunCommand\$($MyInvocation.MyCommand)" | Out-Null
+    }
+    Write-Host+ "[$($MyInvocation.MyCommand)] FINISHED"
+}
+function Test-MessagingOnIntervention { 
+    Write-Host+ "[$($MyInvocation.MyCommand)] PENDING "
 
-    Write-Host+ "[Test-AzureRunCommand] FINISHED"
+    $_platformStatus = Get-PlatformStatus -CacheOnly
+    $_platformStatus.Intervention = $true 
+    $_platformStatus | Write-Cache platformstatus
+
+    Send-TaskMessage -Id Monitor -Status Testing -MessageType $global:PlatformMessageType.Information -Message "Testing information message via AzureRunCommand\$($MyInvocation.MyCommand)" | Out-Null
     
+    Write-Host+ "[$($MyInvocation.MyCommand)] FINISHED"
 }
 
 $global:DebugPreference = "SilentlyContinue"
@@ -60,11 +79,17 @@ if ($Command) {
         $global:Product = @{Id="AzureRunCommand"}
         . $PSScriptRoot\definitions.ps1
 
-        # disable messaging for the specified duration and send message
-        # note that messages of type intervention or from source "Send-MessageStatus" are not affected
-        Disable-Messaging -Duration (New-Timespan -Minutes 90) -Notify  
+        $action = "Remoting to $OverwatchController using CredSSP"; $status = "Start"; $message = "$($action): $status" 
+        Write-Log -Context $Context -Action $action -Target $Command -Status $status -Message $message
+        Write-Host+ -NoTrace $message   
 
-        Write-Host+ -NoTrace "Remoting to $OverwatchController using CredSSP `"double hop`"." 
+        # disable messaging when azure remote command is stop-platform
+        # assumption: azure remote command start-platform will follow stop-platform
+        # messaging will be enabled at end azure remote command start-platform
+        # note: intervention message types are always sent and will re-enable messaging
+        if ($Command.ToLower() -in ("stop-platform","test-messagingonstop")) {
+            Disable-Messaging -Duration (New-Timespan -Minutes 90) -Notify  
+        }
 
         $creds = Get-Credentials "localadmin-$($Platform.Instance)" -LocalMachine
 
@@ -72,10 +97,14 @@ if ($Command) {
         $result = Invoke-Command -ComputerName $OverwatchController `
             -ScriptBlock {
                     Set-Location $using:workingDirectory; 
-                    pwsh azureruncommand.ps1 -Command $using:Command -Context $using:Context -Reason $using:Reason -NoDoubleHop -Credssp -SkipPreflight
+                    pwsh azureruncommand.ps1 -Command $using:Command -Context $using:Context -Reason $using:Reason -NoDoubleHop -CredSSP -SkipPreflight
                 } `
             -Authentication Credssp `
             -Credential $creds 
+
+        $action = "Remoting to $OverwatchController using CredSSP"; $status = "Completed"; $message = "$($action): $status" 
+        Write-Log -Context $Context -Action $action -Target $Command -Status $status -Message $message
+        Write-Host+ -NoTrace $message         
 
     }
     else {
@@ -88,9 +117,9 @@ if ($Command) {
 
         $global:WriteHostPlusPreference = "Continue"
 
-        # disable messaging for the specified duration and send message
-        # note that messages of type intervention or from source "Send-MessageStatus" are not affected
-        Disable-Messaging -Duration (New-Timespan -Minutes 90) -Notify
+        $action = "Execute"; $status = "Start"; $message = "$action $($Command): $status" 
+        Write-Log -Context $Context -Action $action -Target $Command -Status $status -Message $message
+        Write-Host+ -NoTrace $message    
 
         $commandExpression = $Command
         $commandParametersKeys = (Get-Command $Command.Split(" ")[0]).parameters.keys
@@ -105,7 +134,15 @@ if ($Command) {
         
         $result = Invoke-Expression -Command $commandExpression
 
-        Enable-Messaging -Notify
+        $action = "Execute"; $status = "Completed"; $message = "$action $($Command): $status" 
+        Write-Log -Context $Context -Action $action -Target $Command -Status $status -Message $message
+        Write-Host+ -NoTrace $message 
+
+        # enable messaging if needed
+        # note: see disable messaaging above
+        if ($Command.ToLower() -in ("start-platform","test-messagingonstart")) {
+            Enable-Messaging -Notify            
+        }
 
     }
 
