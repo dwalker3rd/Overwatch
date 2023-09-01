d38eb409f8562fffac805fe1a80f1e01a3b782c1
David Walker <dwalker3rd@gmail.com>
2023-09-01 10:02:50 +0000

    typo corrected

--- source/platform/alteryxserver/preflightchecks-platform-alteryxserver-template.ps1
+++ source/platform/alteryxserver/preflightchecks-platform-alteryxserver-template.ps1
@@ -1,7 +1,7 @@
 #region PREFLIGHT
 
-    Test-NetConnection+ -ComputerName (pt -nodes -k) | Out-Null
-    Test-PSRemoting | Out-Null -ComputerName (pt -nodes -k) | Out-Null
+    Test-NetConnection+ -ComputerName (pt nodes -k) | Out-Null
+    Test-PSRemoting | Out-Null -ComputerName (pt nodes -k) | Out-Null
 
     #region Test-SslProtocol
 
--- source/platform/tableaurmt/preflightchecks-platform-tableaurmt-template.ps1
+++ source/platform/tableaurmt/preflightchecks-platform-tableaurmt-template.ps1
@@ -1,7 +1,7 @@
 #region PREFLIGHT
 
-    Test-NetConnection+ -ComputerName (pt -nodes -k) | Out-Null
-    Test-PSRemoting | Out-Null -ComputerName (pt -nodes -k) | Out-Null
+    Test-NetConnection+ -ComputerName (pt nodes -k) | Out-Null
+    Test-PSRemoting | Out-Null -ComputerName (pt nodes -k) | Out-Null
 
     $heartbeat = Get-Heartbeat
     if ($heartbeat.IsOK) {
