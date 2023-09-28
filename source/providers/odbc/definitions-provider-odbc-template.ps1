173c71b13d81e92ba078fa0de8cb26fd97375fe8
David Walker <dwalker3rd@gmail.com>
2023-09-28 15:05:31 +0000

    convert odbc service to provider

---
+++ source/providers/odbc/definitions-provider-odbc-template.ps1
@@ -0,0 +1,22 @@
+#region PROVIDER DEFINITIONS
+
+param(
+    [switch]$MinimumDefinitions
+)
+
+if ($MinimumDefinitions) {
+    $root = $PSScriptRoot -replace "\\definitions",""
+    Invoke-Command  -ScriptBlock { . $root\definitions.ps1 -MinimumDefinitions }
+}
+else {
+    . $PSScriptRoot\classes.ps1
+}
+
+$Provider = $null
+$Provider = $global:Catalog.Provider."ODBC"
+
+$Provider.Config = @{}
+
+return $Provider
+
+#endregion PROVIDER DEFINITIONS
---
+++ source/providers/odbc/install-provider-odbc.ps1
@@ -0,0 +1,29 @@
+param (
+    [switch]$UseDefaultResponses
+)
+
+$Provider = Get-Provider -Id 'ODBC'
+$Id = $Provider.Id 
+
+$interaction = $false
+
+$cursorVisible = [console]::CursorVisible
+Set-CursorVisible
+
+$message = "  $Id$($emptyString.PadLeft(20-$Id.Length," "))","PENDING"
+Write-Host+ -NoTrace -NoTimestamp -NoSeparator -NoNewLine $message.Split(":")[0],$message.Split(":")[1] -ForegroundColor Gray,DarkGray
+
+#region PRODUCT-SPECIFIC INSTALLATION
+#endregion PRODUCT-SPECIFIC INSTALLATION
+
+if ($interaction) {
+    Write-Host+
+    $message = "  $Id$($emptyString.PadLeft(20-$Id.Length," "))","INSTALLED"
+    Write-Host+ -NoTrace -NoTimestamp -NoSeparator $message.Split(":")[0],$message.Split(":")[1] -ForegroundColor Gray,DarkGreen
+}
+else {
+    $message = "$($emptyString.PadLeft(7,"`b"))INSTALLED"
+    Write-Host+ -NoTrace -NoTimestamp -NoSeparator $message -ForegroundColor DarkGreen
+}
+
+[console]::CursorVisible = $cursorVisible
