f8e8e59932aaea96ec8f78196b2131246535254d
David Walker <dwalker3rd@gmail.com>
2023-10-03 04:29:55 +0000

    switch from atstartup to event trigger for task

--- source/product/ssomonitor/install-product-ssomonitor.ps1
+++ source/product/ssomonitor/install-product-ssomonitor.ps1
@@ -9,13 +9,12 @@ $Id = $product.Id
 $message = "  $Id$($emptyString.PadLeft(20-$Id.Length," "))","PENDING$($emptyString.PadLeft(13," "))PENDING$($emptyString.PadLeft(13," "))"
 Write-Host+ -NoTrace -NoTimestamp -NoSeparator -NoNewLine $message[0],$message[1] -ForegroundColor Gray,DarkGray
 
-Copy-File "$($global:Location.Root)\source\product\$($product.Id.ToLower())\definitions-product-$($product.Id.ToLower())-template.ps1" "$($global:Location.Root)\definitions\definitions-product-$($product.Id.ToLower()).ps1" -Quiet
-
 $productTask = Get-PlatformTask -Id "SSOMonitor"
 if (!$productTask) {
+    $subscription = "<QueryList><Query Id='0' Path='System'><Select Path='System'>*[System[Provider[@Name='eventlog' or @Name='Microsoft-Windows-Eventlog' or @Name='User32'] and (EventID=6005)]]</Select></Query></QueryList>"  
     Register-PlatformTask -Id "SSOMonitor" -execute $pwsh -Argument "$($global:Location.Scripts)\$("SSOMonitor").ps1" -WorkingDirectory $global:Location.Scripts `
         -Once -At $(Get-Date).AddSeconds(5) `
-        -AtStartup -Delay $(New-TimeSpan -Minutes 1) `
+        -Subscription $subscription `
         -ExecutionTimeLimit $(New-TimeSpan -Seconds 0) -RunLevel Highest -Start
     $productTask = Get-PlatformTask -Id "SSOMonitor"
 }
