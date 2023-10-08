5c7728f650a99736c4f4e7486963c97a7f400d40
David Walker <dwalker3rd@gmail.com>
2023-10-08 06:52:52 +0000

    remove token from template

--- source/providers/onepassword/definitions-provider-onepassword-template.ps1
+++ source/providers/onepassword/definitions-provider-onepassword-template.ps1
@@ -14,13 +14,15 @@ else {
 
 $Provider = $null
 $Provider = $global:Catalog.Provider."OnePassword"
-$Provider.Config = @{}
+$Provider.Config = @{
+    RegexPattern = @{
+        ErrorMessage = "^\[(.*)\]\s*(\d{4}\/\d{2}\/\d{2}\s*\d{2}\:\d{2}\:\d{2})\s*(.*)$"
+    }
+}
 
-$env:OP_SERVICE_ACCOUNT_TOKEN = "<op_service_account_token>"
+$env:OP_SERVICE_ACCOUNT_TOKEN = "<serviceAccountToken>"
 $env:OP_FORMAT = "json"
-
-$global:VaultVersion = $Provider.Id
 
 return $Provider
 
-#endregion PROVIDER DEFINITIONS
+#endregion PROVIDER DEFINITION
