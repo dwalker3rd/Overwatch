e02b1f81b44008e93fd51c35910c65f0332fa4a1
David Walker <dwalker3rd@gmail.com>
2023-10-09 15:51:50 +0000

    change how ssomonitor triggers/starts

--- source/product/ssomonitor/install-product-ssomonitor.ps1
+++ source/product/ssomonitor/install-product-ssomonitor.ps1
@@ -7,10 +7,8 @@ $_product | Out-Null
 
 $productTask = Get-PlatformTask -Id "SSOMonitor"
 if (!$productTask) {
-    $subscription = "<QueryList><Query Id='0' Path='System'><Select Path='System'>*[System[Provider[@Name='eventlog' or @Name='Microsoft-Windows-Eventlog' or @Name='User32'] and (EventID=6005)]]</Select></Query></QueryList>"  
     Register-PlatformTask -Id "SSOMonitor" -execute $pwsh -Argument "$($global:Location.Scripts)\$("SSOMonitor").ps1" -WorkingDirectory $global:Location.Scripts `
         -Once -At $(Get-Date).AddSeconds(5) `
-        -Subscription $subscription `
         -ExecutionTimeLimit $(New-TimeSpan -Seconds 0) -RunLevel Highest -Start
     $productTask = Get-PlatformTask -Id "SSOMonitor"
 }
