909f0ea50bb26a6ad2b88c5f2da8cb34a97b3575
David Walker <dwalker3rd@gmail.com>
2023-10-18 08:09:56 +0000

    indent

--- source/providers/onepassword/definitions-provider-onepassword-template.ps1
+++ source/providers/onepassword/definitions-provider-onepassword-template.ps1
@@ -1,31 +1,31 @@
 #region PROVIDER DEFINITIONS
 
-param(
-    [switch]$MinimumDefinitions
-)
+    param(
+        [switch]$MinimumDefinitions
+    )
 
-if ($MinimumDefinitions) {
-    $root = $PSScriptRoot -replace "\\definitions",""
-    Invoke-Command  -ScriptBlock { . $root\definitions.ps1 -MinimumDefinitions }
-}
-else {
-    . $PSScriptRoot\classes.ps1
-}
-
-$Provider = $null
-$Provider = $global:Catalog.Provider."OnePassword"
-$Provider.Config = @{
-    RegexPattern = @{
-        ErrorMessage = "^\[(.*)\]\s*(\d{4}\/\d{2}\/\d{2}\s*\d{2}\:\d{2}\:\d{2})\s*(.*)$"
+    if ($MinimumDefinitions) {
+        $root = $PSScriptRoot -replace "\\definitions",""
+        Invoke-Command  -ScriptBlock { . $root\definitions.ps1 -MinimumDefinitions }
     }
-}
+    else {
+        . $PSScriptRoot\classes.ps1
+    }
 
-# switch to Overwatch vault service to get/set the 1Password service account token
-. "$($global:Location.Services)\vault.ps1"
+    $Provider = $null
+    $Provider = $global:Catalog.Provider."OnePassword"
+    $Provider.Config = @{
+        RegexPattern = @{
+            ErrorMessage = "^\[(.*)\]\s*(\d{4}\/\d{2}\/\d{2}\s*\d{2}\:\d{2}\:\d{2})\s*(.*)$"
+        }
+    }
 
-$env:OP_SERVICE_ACCOUNT_TOKEN = (Get-Credentials OP-SERVICE-ACCOUNT-TOKEN).GetNetworkCredential().Password
-$env:OP_FORMAT = "json"
+    # switch to Overwatch vault service to get/set the 1Password service account token
+    . "$($global:Location.Services)\vault.ps1"
 
-return $Provider
+    $env:OP_SERVICE_ACCOUNT_TOKEN = (Get-Credentials OP-SERVICE-ACCOUNT-TOKEN).GetNetworkCredential().Password
+    $env:OP_FORMAT = "json"
+
+    return $Provider
 
 #endregion PROVIDER DEFINITION
