a74e27f1201ef2ee87ad1b1377730f613494d962
David Walker <dwalker3rd@gmail.com>
2023-11-28 13:17:35 +0000

    $global:Location updates

--- source/environ/environ-template.ps1
+++ source/environ/environ-template.ps1
@@ -1,47 +1,42 @@
 #region ENVIRON
 
-    $global:Environ = @{
-        Overwatch = "Overwatch"
-        OS = "<operatingSystemId>"
-        Cloud = "<cloudId>"
-        Platform = "<platformId>"
-        Instance = "<platformInstanceId>"
-        Product = @(<productIds>)
-        Provider = @(<providerIds>)
-    }
+$global:Environ = @{
+    Overwatch = "Overwatch"
+    OS = "WindowsServer"
+    Cloud = "Azure"
+    Platform = "TableauServer"
+    Instance = "tableautest-path-org"
+    Product = @("AzureADCache", "AzureADSyncTS", "AzureProjects", "AzureUpdateMgmt", "Cleanup", "Command", "DiskCheck", "Monitor")
+    Provider = @("AzureAD", "MicrosoftTeams", "Okta", "OnePassword", "Postgres", "SMTP", "TableauServerRestApi", "TableauServerTabCmd", "TableauServerTsmApi", "TableauServerWC", "TwilioSMS", "Views")
+}
 
 #endregion ENVIRON
 #region LOCATION
 
-    $global:Location = @{
-        Root = "<overwatchInstallLocation>"
-        Images = "<imagesURI>"
-    }
-    $global:Location += @{
-        Archive = "$($global:Location.Root)\archive"
-        Config = "$($global:Location.Root)\config"
-        Credentials = "$($global:Location.Root)\data"
-        Data = "$($global:Location.Root)\data"
-        Definitions = "$($global:Location.Root)\definitions"
-        Help = "$($global:Location.Root)\help"
-        Initialize = "$($global:Location.Root)\initialize"
-        Install = "$($global:Location.Root)\install"
-        Logs = "$($global:Location.Root)\logs"
-        Postflight = "$($global:Location.Root)\postflight"
-        Preflight = "$($global:Location.Root)\preflight"
-        Providers = "$($global:Location.Root)\providers"
-        Scripts = "$($global:Location.Root)"
-        Services = "$($global:Location.Root)\services"
-        Source = "$($global:Location.Root)\source"
-        Temp = "$($global:Location.Root)\temp"
-        Views = "$($global:Location.Root)\views"
+$global:Location = @{
+    Root = "F:\Overwatch"
+    Images = "https://pathai4healthusers.z33.web.core.windows.net/Overwatch/img"
 }
+$global:Location += @{
+    Archive = "$($global:Location.Root)\archive"
+    Config = "$($global:Location.Root)\config"
+    Credentials = "$($global:Location.Root)\data"
+    Data = "$($global:Location.Root)\data"
+    Definitions = "$($global:Location.Root)\definitions"
+    Help = "$($global:Location.Root)\help"
+    Initialize = "$($global:Location.Root)\initialize"
+    Install = "$($global:Location.Root)\install"
+    Logs = "$($global:Location.Root)\logs"
+    Postflight = "$($global:Location.Root)\postflight"
+    Preflight = "$($global:Location.Root)\preflight"
+    Providers = "$($global:Location.Root)\providers"
+    Scripts = "$($global:Location.Root)"
+    Services = "$($global:Location.Root)\services"
+    Source = "$($global:Location.Root)\source"
+    Temp = "$($global:Location.Root)\temp"
+    Views = "$($global:Location.Root)\views"
+}
+$global:Location += @{ Classes = "$($global:Location.Definitions)\classes.ps1"}
+$global:Location += @{ Catalog = "$($global:Location.Definitions)\catalog.ps1"}
 
-#endregion LOCATION     
-
-
-
-
-
-
-
+#endregion LOCATION
