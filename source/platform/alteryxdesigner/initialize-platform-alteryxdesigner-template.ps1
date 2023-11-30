ce0b745f728f90de1734eb563dd771ffb33142d7
David Walker <dwalker3rd@gmail.com>
2023-11-30 07:48:33 +0000

    new alteryxdesigner platform

---
+++ source/platform/alteryxdesigner/initialize-platform-alteryxdesigner-template.ps1
@@ -0,0 +1,25 @@
+#region INIT
+
+    Write-Host+
+    $message = "Platform Initialization"
+    $leader = Format-Leader -Length 46 -Adjust $message.Length
+    Write-Host+ -NoTrace -NoNewline $message,$leader -ForegroundColor DarkBlue,DarkGray
+
+    try {
+
+        Get-PlatformInfo
+
+    }
+    catch {
+        Write-Host+ -NoTrace -NoTimestamp "FAIL" -ForegroundColor DarkRed 
+        Write-Log -Action "Initialize" -Target "Platform" -Status "Fail" -EntryType "Error"
+        # throw "[$([datetime]::Now)] Initialize platform ... FAIL"
+        return
+    }
+
+    Write-Host+ -NoTrace -NoTimestamp "SUCCESS" -ForegroundColor DarkGreen 
+    Write-Host+
+    
+    return
+
+#endregion INIT
