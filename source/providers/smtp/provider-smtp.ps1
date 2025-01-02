2c7229e91156a259b48b47c06958ebde6ff8d1ed
David Walker <dwalker3rd@gmail.com>
2025-01-02 13:50:01 +0000

    mod smtp provider to call send-smtp directly

--- source/providers/smtp/provider-smtp.ps1
+++ source/providers/smtp/provider-smtp.ps1
@@ -28,9 +28,10 @@ function global:Send-SMTP {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory=$false)][string]$From,
-        [Parameter(Mandatory=$false)][string]$To, 
+        [Parameter(Mandatory=$false)][string[]]$To, 
         [Parameter(Mandatory=$false)][string]$Subject,
         [Parameter(Mandatory=$false)][string]$Body,
+        [Parameter(Mandatory=$false)][ValidateSet("HTML", "Text")][string]$BodyFormat = "HTML",
         [Parameter(Mandatory=$false)][object]$Message,
         [Parameter(Mandatory=$false)][string]$json
     )
@@ -40,26 +41,36 @@ function global:Send-SMTP {
     $provider = get-provider -id 'SMTP'
     $providerCredentials = Get-Credentials $provider.Id
     $From = ![string]::IsNullOrEmpty($From) ? $From : (![string]::IsNullOrEmpty($provider.Config.From) ? $provider.Config.From : $providerCredentials.UserName)
-    if (!$To) {$To = $(get-contact).Email}
+    if (!$To) {$To = [array]($(get-contact).Email)}
     if (!$Subject) {$Subject = $Message.Subject}
 
     $builder = New-Object MimeKit.BodyBuilder
-    foreach ($section in $Message.Sections) {
-        foreach ($fact in $section.Facts) {
-            $tab = Format-Leader -Character " " -Length 20 -Adjust ((("$($fact.name):").Length))
-            $builder.TextBody += $fact.name ? "$($fact.name):$($tab)$($fact.value)`n" : $fact.value
-            $builder.HtmlBody += $fact.name ? "<pre><b>$($fact.name)</b>:$($tab)$($fact.value)</pre>" : "<pre>$($fact.value)</pre>"
+    if (![string]::IsNullOrEmpty($Body)) {
+        switch ($BodyFormat) {
+            "HTML" {$builder.HtmlBody += $Body}
+            "Text" {$builder.TextBody += $Body}
+        }
+    }
+    else {
+        foreach ($section in $Message.Sections) {
+            foreach ($fact in $section.Facts) {
+                $tab = Format-Leader -Character " " -Length 20 -Adjust ((("$($fact.name):").Length))
+                $builder.TextBody += $fact.name ? "$($fact.name):$($tab)$($fact.value)`n" : $fact.value
+                $builder.HtmlBody += $fact.name ? "<pre><b>$($fact.name)</b>:$($tab)$($fact.value)</pre>" : "<pre>$($fact.value)</pre>"
+            }
         }
     }
 
     $SMTP = New-Object MailKit.Net.Smtp.SmtpClient
     $SMTPMessage = New-Object MimeKit.MimeMessage
     $SMTPMessage.From.Add($From)
-    $SMTPMessage.To.Add($To)
+    foreach ($emailAddress in $To) {
+        $SMTPMessage.To.Add($emailAddress)
+    }
     $SMTPMessage.Subject = $Subject
     $SMTPMessage.Body = $builder.ToMessageBody()
 
-    $logEntry = read-log $provider.Id -Context "Provider.SMTP" -Action $To -Status $global:PlatformMessageStatus.Transmitted -Message $Message.Summary -Newest 1
+    $logEntry = read-log $provider.Id -Context "Provider.SMTP" -Action $To[0] -Status $global:PlatformMessageStatus.Transmitted -Message $Message.Summary -Newest 1
     $throttle = $logEntry -and $logEntry.Message -eq $Message.Summary ? ([datetime]::Now - $logEntry.TimeStamp).TotalSeconds -le $Message.Throttle.TotalSeconds : $null
 
     if (!$throttle) {
@@ -70,7 +81,7 @@ function global:Send-SMTP {
         $SMTP.Dispose()
     }
     
-    Write-Log -Context "Provider.SMTP" -Name $provider.Id -Action $To -Message $Message.Summary -Status $($throttle ? $global:PlatformMessageStatus.Throttled : $global:PlatformMessageStatus.Transmitted) -Force
+    Write-Log -Context "Provider.SMTP" -Name $provider.Id -Action $To[0] -Message $Message.Summary -Status $($throttle ? $global:PlatformMessageStatus.Throttled : $global:PlatformMessageStatus.Transmitted) -Force
 
     return $throttle ? $global:PlatformMessageStatus.Throttled : $global:PlatformMessageStatus.Transmitted
 
