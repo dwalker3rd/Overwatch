59188ae5516b49d6899d06c199e4e702a61072ca
David Walker <dwalker@path.org>
2025-09-05 09:10:23 +0000

    updated Get-SharePointListItems; added Remove-SharePointListItems

--- source/providers/sharepoint/provider-sharepoint.ps1
+++ source/providers/sharepoint/provider-sharepoint.ps1
@@ -33,25 +33,71 @@ function global:Get-SharePointSiteListItems {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$true,Position=0)][object]$Site,
-        [Parameter(Mandatory=$true,Position=1)][object]$List
+        [Parameter(Mandatory=$true,Position=1)][object]$List,
+        [Parameter(Mandatory=$false)][string[]]$Column,
+        [Parameter(Mandatory=$false)][string[]]$ExcludeColumn
     )   
 
-    $columns = Get-MgBetaSiteListColumn -SiteId $Site.Id -ListId $List.Id |
-        Where-Object { -not $_.Hidden -and -not $_.ReadOnly -and $_.ColumnGroup -ne "_Hidden"}
-    $columnDisplayNames = $columns.DisplayName
+    if ($Column -and $ExcludeColumn) {
+        throw "The parameters -Column and -ExcludeColumn cannot be used together."
+    }
 
-    $listItems = Get-MgBetaSiteListItem -SiteId $site.Id -ListId $List.Id -Property 'id,fields' -ExpandProperty 'fields'
+    $_columns = Get-MgBetaSiteListColumn -SiteId $Site.Id -ListId $List.Id |
+        Where-Object { (-not $_.Hidden -and -not $_.ReadOnly -and $_.ColumnGroup -ne "_Hidden") -or $_.DisplayName -eq "ID" }
+    if ($Column) {
+        $_columns = $_columns | Where-Object { $_.DisplayName -in $Column }
+    }
+    if ($ExcludeColumn) {
+        $_columns = $_columns | Where-Object { $_.DisplayName -notin $ExcludeColumn }
+    }
+    $columnDisplayNames = $_columns.DisplayName
+
+    $listItems = Get-MgBetaSiteListItem -SiteId $site.Id -ListId $List.Id -Property 'id, createdDateTime, lastModifiedDateTime, fields' -ExpandProperty 'fields'
 
     $listItemData = foreach ($listItem in $listItems) {
         $_listItemData = [ordered]@{}
         foreach ($columnDisplayName in $columnDisplayNames) {
             # map displayName -> columnName
-            $columnName = ($columns | Where-Object DisplayName -eq $columnDisplayName).Name
-            $_listItemData[$columnDisplayName] = $listItem.Fields.AdditionalProperties[$columnName]
+            $columnName = ($_columns | Where-Object { $_.DisplayName -eq $columnDisplayName }).Name
+            if ($listItem.$columnDisplayName) {
+                if ($columnDisplayName -in @("id", "createdDateTime", "lastModifiedDateTime")) {
+                    $_listItemData[$columnDisplayName] = [int]$listItem.$columnDisplayName
+                }
+                else {
+                    $_listItemData[$columnDisplayName] = $listItem.$columnDisplayName
+                }
+            }
+            else {
+                if ("$($columnName)LookupId" -in $listItem.Fields.AdditionalProperties.Keys) {
+                    $columnLookupListId = ($_columns | Where-Object { $_.DisplayName -eq $columnDisplayName }).Lookup.ListId
+                    $columnLookupColumnName = ($_columns | Where-Object { $_.DisplayName -eq $columnDisplayName }).Lookup.ColumnName                    
+                    $lookupValueListItem = Get-MgBetaSiteListItem -SiteId $Site.Id -ListId $columnLookupListId -ListItemId $listItem.Fields.AdditionalProperties."$($columnName)LookupId"
+                    $lookupValue = $lookupValueListItem.Fields.AdditionalProperties.$columnLookupColumnName
+                    $_listItemData[$columnDisplayName] = $lookupValue
+                    # $_listItemData["$($columnName)LookupId"] = [int]$listItem.Fields.AdditionalProperties["$($columnName)LookupId"]
+                }
+                else {
+                    $_listItemData[$columnDisplayName] = $listItem.Fields.AdditionalProperties[$columnName]
+                }
+            }
         }
         [pscustomobject]$_listItemData
     }
 
     return $listItemData
 
-}    
+}
+
+function global:Remove-SharePointSiteListItem {
+
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true,Position=0)][object]$Site,
+        [Parameter(Mandatory=$true,Position=1)][object]$List,
+        [Parameter(Mandatory=$true)][object]$ListItem
+    ) 
+    
+    Remove-MgBetaSiteListItem -SiteId $Site.Id -ListId $List.Id -ListItemId $ListItem.Id
+
+}
